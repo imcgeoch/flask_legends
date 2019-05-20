c378ba19e62d6ae1aaf3993e9af457b5061ff093
Ian McGeoch <imcgeoch@cs.umass.edu>
2019-05-19 21:07:19 -0400

    add world and artifacts

--- legends/models.py
+++ legends/models.py
@@ -2,3 +2,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class DF_World(db.Model):
+    __tablename__ = 'df_world'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    historical_figures = db.relationship('Historical_Figure', backref='df_world')
+    artifacts = db.relationship('Artifact', backref='df_world')
+
+    def __repr__(self):
+        return "<df_world %s>" % (id)
+
+class Historical_Figure(db.Model):
+    __tablename__ = 'historical_figures'
+
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'),
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    
+    held_artifacts = db.relationship('Artifact', backref='holder_hf', 
+                                      viewonly=True)
+
+class Artifact(db.Model):
+    __tablename__ = 'artifacts'
+
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    holder_hfid = db.Column(db.Integer)
+    site_id = db.Column(db.Integer)
+    structure_local_id = db.Column(db.Integer)
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, holder_hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {})
+
+    def __repr__(self):
+        return "<Artifact %s>" % (self.name)
