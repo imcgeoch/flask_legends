b97097a920167f7d95a31e25cdf644d645a56cd4
Ian McGeoch <imcgeoch@cs.umass.edu>
2019-05-25 23:37:42 -0400

    MORE models

--- legends/models.py
+++ legends/models.py
@@ -1,3 +1,5 @@
+import enum
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -49,6 +51,27 @@ entity_reputations = db.Table('entity_reputations', db.metadata,
 
 
 
+
+link_types = ['child', 'spouse', 'deity']
+'''
+hf_links = db.Table('hf_links', db.metadata,
+        db.Column('id', db.Integer, primary_key=True),
+        db.Column('df_world_id', db.Integer),
+        db.Column('hfid1', db.Integer),
+        db.Column('hfid2', db.Integer),
+        db.Column('link_strength', db.Integer),
+        db.Column('link_type', db.Enum('child','spouse')),
+
+        db.ForeignKeyConstraint(['df_world_id', 'hfid1'],
+                                ['historical_figures.df_world_id',
+                                 'historical_figures.id']),
+        db.ForeignKeyConstraint(['df_world_id', 'hfid2'],
+                                ['historical_figures.df_world_id',
+                                 'historical_figures.id'])
+        )
+'''
+
+
 class DF_World(db.Model):
     __tablename__ = 'df_world'
     id = db.Column(db.Integer, primary_key=True)
@@ -75,6 +98,8 @@ class Entity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
+    site_links = db.relationship('Site_Link', backref='entity', 
+                                 viewonly=True)
 
 class Historical_Figure(db.Model):
     __tablename__ = 'historical_figures'
@@ -115,8 +140,172 @@ class Historical_Figure(db.Model):
                              backref='reputation_holders',
                              viewonly=True)
 
+    hf_links = db.relationship('HF_Link', backref='this_histfig',
+            foreign_keys='HF_Link.hfid1,HF_Link.df_world_id')
+    hf_relationships = db.relationship('Relationship', 
+            backref='this_histfig',
+            foreign_keys='Relationship.hfid1,Relationship.df_world_id')
+    
+    site_links = db.relationship('Site_Link', backref='historical_figure', 
+                                 viewonly=True)
+
+    skills = db.relationship('Skill')
+
+    interaction_knowledges = db.relationship('Interaction_Knowledge')
+    journey_pets = db.relationship('Journey_Pet')
+    spheres = db.relationship('Sphere')
+
     def __repr__(self):
         return "<Historical Figure %s>" % (self.name)
+
+class Journey_Pet(db.Model):
+    __tablename__ = 'journey_pets'
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid = db.Column(db.Integer)
+    journey_pet = db.Column(db.String(20))
+
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {}) 
+class Skill(db.Model):
+    __tablename__ = 'skills'
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid = db.Column(db.Integer)
+    skill = db.String(20) # should prolly be enum
+    total_ip = db.Column(db.Integer)
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {}) 
+                                 
+class Interaction_Knowledge(db.Model):
+    __tablename__ = 'interaction_knowledges'
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid = db.Column(db.Integer)
+    interaction_knowledge = db.Column(db.String(12)) #what is the meaning?
+    
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {}) 
+
+class HF_Link(db.Model):
+    __tablename__ = 'hf_links'
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid1 = db.Column(db.Integer)
+    hfid2 = db.Column( db.Integer)
+    link_strength = db.Column(db.Integer)
+    link_type = db.Column(db.Enum('child','spouse'))
+
+    other = db.relationship("Historical_Figure", 
+            primaryjoin="and_(HF_Link.hfid2==Historical_Figure.id," + 
+                "HF_Link.df_world_id==Historical_Figure.df_world_id)")
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid1],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), 
+                      db.ForeignKeyConstraint([df_world_id, hfid2],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), 
+                      {})
+
+class Site_Link(db.Model):
+    __tablename__ = 'site_links'
+
+    types = ['lair', 'home structure', 'seat of power', 'occupation']
+
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid = db.Column(db.Integer)
+    entity_id = db.Column(db.Integer)
+    link_type = db.Column(db.Enum(*types))
+    occupation_id = db.Column(db.Integer)
+    site_id = db.Column(db.Integer)
+    sub_id = db.Column(db.Integer)
+    
+    __table_args__ =( 
+        db.ForeignKeyConstraint(['df_world_id', 'hfid'],
+                                ['historical_figures.df_world_id',
+                                 'historical_figures.id']),
+        db.ForeignKeyConstraint(['df_world_id', 'entity_id'],
+                                ['entities.df_world_id', 'entities.id']),
+        db.ForeignKeyConstraint(['df_world_id', 'site_id'],
+                                ['sites.df_world_id', 'sites.id'],
+                                ), 
+        {})
+
+class Relationship(db.Model):
+    __tablename__ = 'relationship'
+    id = db.Column(db.Integer, primary_key=True)
+    df_world_id = db.Column(db.Integer)
+    hfid1 = db.Column(db.Integer)
+    hfid2 = db.Column(db.Integer)
+    known_identity_id = db.Column(db.Integer)
+    last_meet_seconds72 = db.Column(db.Integer)
+    last_meet_year = db.Column(db.Integer)
+    meet_count = db.Column(db.Integer)
+    rep = db.Column(db.Integer) # only one of buddy, info source ever used
+
+    other = db.relationship("Historical_Figure", primaryjoin=
+    "and_(Relationship.hfid2==Historical_Figure.id," + 
+    "Relationship.df_world_id==Historical_Figure.df_world_id)")
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid1],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), 
+                      db.ForeignKeyConstraint([df_world_id, hfid2],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), 
+                      {})
+
+
+
+class Goal(db.Model):
+    __tablename__ = 'goals'
+    
+    goals = ['create a great work of art', 'immortality', 'master a skill',
+             'start a family', 'rule the world', 'fall in love',
+             'see the great natural sites', 'become a legendary warrior',
+             'bring peace to the world', 'make a great discovery']
+
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    hfid = db.Column(db.Integer, primary_key=True)
+    goal = db.Column(db.Enum(*goals), primary_key=True)
+
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {})
+
+class Sphere(db.Model):
+    __tablename__ = 'spheres'
+
+    spheres = ['fire', 'wealth', 'caverns', 'misery', 'water', 
+               'deformity', 'depravity', 'death', 'thralldom', 
+               'chaos', 'blight', 'disease', 'treachery', 'suicide', 
+               'nightmares', 'torture', 'animals', 'nature', 
+               'plants', 'rivers', 'jewels', 'minerals', 'metals', 
+               'mountains', 'fortresses', 'war', 'wisdom', 
+               'scholarship', 'writing', 'victory', 'volcanos', 
+               'night', 'trickery', 'lies', 'oceans', 'speech', 
+               'theft', 'forgiveness', 'mercy', 'creation', 'birth', 
+               'youth', 'longevity', 'song', 'truth', 'crafts', 
+               'family', 'children', 'murder', 'healing', 
+               'lightning', 'dreams', 'rumors', 'fish', 'fishing', 
+               'hunting', 'strength', 'light', 'thunder', 'food']
+    
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    hfid = db.Column(db.Integer, primary_key=True)
+    sphere = db.Column(db.Enum(*spheres), primary_key=True)
+    
+    __table_args__ = (db.ForeignKeyConstraint([df_world_id, hfid],
+                                 [Historical_Figure.df_world_id,
+                                  Historical_Figure.id]), {})
 
 class Artifact(db.Model):
     __tablename__ = 'artifacts'
@@ -125,6 +314,10 @@ class Artifact(db.Model):
                             primary_key=True)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    name_string = db.Column(db.String(50))
+    page_number = db.Column(db.Integer)
+    # page_written_content_id = db.Column(db.Integer) redundant scroll vs book
+    written_content_id = db.Column(db.Integer)
     holder_hfid = db.Column(db.Integer)
     site_id = db.Column(db.Integer)
     structure_local_id = db.Column(db.Integer)
@@ -132,6 +325,8 @@ class Artifact(db.Model):
     __table_args__ = (db.ForeignKeyConstraint([df_world_id, holder_hfid],
                                  [Historical_Figure.df_world_id,
                                   Historical_Figure.id]), {})
+
+    # add lookup to written_contents
 
     def __repr__(self):
         return "<Artifact %s>" % (self.name)
@@ -144,22 +339,6 @@ class Historical_Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
 
-
-'''
-class Competitor(db.Model):
-    __tablename__ = 'competitors'
-
-    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
-                            primary_key=True)
-    event_id = db.Column(db.Integer, primary_key=True) 
-    competitor_hfid = db.Column(db.Integer, primary_key=True)
-
-    __table_args__ = (db.ForeignKeyConstraint(
-                         [df_world_id, competitor_hfid],
-                         [Historical_Figure.df_world_id,
-                         Historical_Figure.id]), {})
-'''
-
 class Historical_Era(db.Model):
     __tablename__ = 'historical_eras'
 
@@ -171,3 +350,47 @@ class Historical_Era(db.Model):
     def __repr__(self):
         return "<Historical Era %s>" % (self.name)
 
+class Region(db.Model):
+    __tablename__ = 'regions'
+
+    types = ['Wetland', 'Grassland', 'Hills', 'Desert', 'Forest',
+             'Mountains', 'Lake', 'Ocean', 'Tundra', 'Glacier']
+
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    type = db.Column(db.Enum(*types))
+
+class Site(db.Model):
+    __tablename__ = 'sites'
+
+    types = ['cave', 'fortress', 'dark fortress', 'forest retreat',
+             'town', 'vault', 'hillocks', 'dark pits', 'hamlet',
+            'tomb', 'mountain halls', 'camp', 'lair', 'shrine']
+
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    coords = db.Column(db.String(10))
+    rectangle = db.Column(db.String(20))
+    type = db.Column(db.Enum(*types))
+
+    site_links = db.relationship('Site_Link', backref='site', viewonly=True)
+
+class Musical_Form(db.Model):
+    __tablename__ = 'musical_forms'
+    
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text())
+
+class Poetic_Form(db.Model):
+    __tablename__ = 'poetic_forms'
+    
+    df_world_id = db.Column(db.Integer, db.ForeignKey('df_world.id'), 
+                            primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text())
