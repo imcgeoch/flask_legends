4f6b23810ec854515d0a0144024bb5b251fb7315
Ian McGeoch <imcgeoch@cs.umass.edu>
2019-07-05 16:30:17 -0400

    further flesh out histfig template

--- legends/explorer/views.py
+++ legends/explorer/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request
 
+from sqlalchemy import or_
+
 from ..models import *
 from . import bp
 
@@ -17,9 +19,28 @@ def world_index():
 
 @bp.route('/<world_id>/histfigs')
 def hf_index(world_id):
-    after = request.args.get('after')
+    after = request.args.get('after') or 0
     hfs = Historical_Figure.query\
                            .filter_by(df_world_id=world_id)\
-                           .limit(1000)\
-                           .offset(after).all()
-    return render_template('histfigs.html', hfs=hfs)
+                           .limit(250).offset(after)
+
+    return render_template('histfig_list.html', items=hfs)
+
+@bp.route('/<world_id>/histfig/<hfid>')
+def hf_detail(world_id, hfid):
+    evt_after = request.args.get('evt_after') or 0
+    hf = Historical_Figure.query\
+                          .filter_by(df_world_id=world_id, id=hfid)\
+                          .first()
+    pronoun = 'he' if hf.caste == 'MALE' else 'she'
+    posessive = 'his' if hf.caste == 'MALE' else 'her'
+    
+    events = Historical_Event.query\
+                             .filter(Historical_Event.df_world_id == world_id,
+                                        or_(Historical_Event.hfid==hfid, 
+                                            Historical_Event.hfid2==hfid))\
+                             .limit(250).offset(evt_after)
+
+    return render_template('histfig_detail.html', hf=hf, 
+                           pronoun=pronoun, posessive=posessive,
+                           events=events.all(), rendered_events=rendered_events)
