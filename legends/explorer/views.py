6a94693d115d5114aa16eb4ec981c395409c3b61
Ian McGeoch <imcgeoch@cs.umass.edu>
2019-07-14 17:00:57 -0400

    detail view for entities closes #55

--- legends/explorer/views.py
+++ legends/explorer/views.py
@@ -39,3 +39,16 @@ def hf_detail(world_id, hfid):
     return render_template('histfig_detail.html', hf=hf, 
                            pronoun=pronoun, posessive=posessive,
                            events=events, rendered_events=[])
+
+@bp.route('/<world_id>/entity/<entity_id>')
+def entity_detail(world_id, entity_id):
+    context = {}
+    evt_after = request.args.get('evt_after') or 0
+    context['entity'] = Entity.query\
+                              .filter_by(df_world_id=world_id, id=entity_id)\
+                              .first()
+    return render_template('entity_detail.html', context=context)
+
+@bp.route('/<world_id>/site/<site_id>')
+def site_detail(world_id, site_id):
+    return "placeholder for site %s" % (site_id)
